0788e03051eeea4b91dc6503ec81a1dda51def35
Drop-Table-Users <avalach84@gmail.com>
2026-02-02 12:20:55 +0100

    Parse block txs

--- src/pages/Block.tsx
+++ src/pages/Block.tsx
@@ -1,13 +1,27 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { fetchBlock } from "../api/block";
+import { fetchTransaction } from "../api/transaction";
+import { parseCbor } from "../utils";
 
 export default function BlockPage() {
   const { headerHash } = useParams();
 
   useEffect(() => {
     if (!headerHash) return;
-    fetchBlock(headerHash).then((data) => console.log(data));
+    fetchBlock(headerHash)
+      .then(async (data) => {
+        const txIds = data.hashes;
+        const transactions = await Promise.all(
+          txIds.map((txId: string) => fetchTransaction(txId)),
+        );
+
+        const parsedTransactions = transactions.map((tx) =>
+          parseCbor(tx.tx),
+        );
+        console.log(parsedTransactions);
+      })
+      .catch((error) => console.error(error));
   }, [headerHash]);
 
   return <div></div>;
